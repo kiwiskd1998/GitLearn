6bf58fac3013d94c95d65aac7a785536d914bc55
cody <kiwiskd1998@163.com>
2022-11-03 16:35:34 +0800

    before merge

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main(){
 
-	cout<<"hello world"<<endl;
-	cout<<"dev test"<<endl;
+//	cout<<"hello world"<<endl;
+	cout<<"1+2=3"<<endl;
+//	cout<<"dev test"<<endl;
 	return 0;
 }
 
